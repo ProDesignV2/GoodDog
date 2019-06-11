578d3d877190e97a1ac5cad3a21513a3fb86ad66
ProDesignV2 <eoinpgogarty@gmail.com>
2019-06-11 02:51:08 +0100

    Main screen set up with dog gif and photo input with reset

--- app/src/main/java/com/avantgarda/gooddog/MainActivity.java
+++ app/src/main/java/com/avantgarda/gooddog/MainActivity.java
@@ -9,11 +9,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
     ImageView imageView;
+    View dudLeft, dudRight;
+
+    private final static int REQUEST_IMAGE_CAPTURE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +26,26 @@ public class MainActivity extends AppCompatActivity {
 
         button = findViewById(R.id.button);
         imageView = findViewById(R.id.imageView);
+        dudLeft = findViewById(R.id.dudLeft);
+        dudRight = findViewById(R.id.dudRight);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent,0);
+                if(intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+
+        button.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                dudLeft.setVisibility(View.VISIBLE);
+                dudRight.setVisibility(View.VISIBLE);
+                imageView.setImageResource(R.drawable.corgi);
+                return true;
             }
         });
     }
@@ -36,9 +54,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == 0){
+        if(requestCode == REQUEST_IMAGE_CAPTURE){
             if(resultCode == RESULT_OK){
                 Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                dudLeft.setVisibility(View.GONE);
+                dudRight.setVisibility(View.GONE);
                 imageView.setImageBitmap(bitmap);
             }
         }
