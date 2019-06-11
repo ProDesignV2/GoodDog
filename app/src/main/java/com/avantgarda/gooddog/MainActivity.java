3ae6e37abb69c6907c81ca3719fa91f921c4d3fc
ProDesignV2 <eoinpgogarty@gmail.com>
2019-06-11 14:53:28 +0100

    Added ML capabilities

--- app/src/main/java/com/avantgarda/gooddog/MainActivity.java
+++ app/src/main/java/com/avantgarda/gooddog/MainActivity.java
@@ -3,13 +3,23 @@ package com.avantgarda.gooddog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -60,6 +70,29 @@ public class MainActivity extends AppCompatActivity {
                 dudLeft.setVisibility(View.GONE);
                 dudRight.setVisibility(View.GONE);
                 imageView.setImageBitmap(bitmap);
+                FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+                FirebaseVisionImageLabeler labeler = FirebaseVision.getInstance()
+                        .getOnDeviceImageLabeler();
+                labeler.processImage(image)
+                        .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
+                            @Override
+                            public void onSuccess(List<FirebaseVisionImageLabel> labels) {
+                                // Task completed successfully
+                                for (FirebaseVisionImageLabel label: labels) {
+                                    String text = label.getText();
+                                    String entityId = label.getEntityId();
+                                    float confidence = label.getConfidence();
+                                    Log.i("MLResult", "onSuccess: " + text + " " + entityId + " " + confidence);
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                Log.i("MLResult", "onFailure: " + e.getMessage());
+                            }
+                        });
             }
         }
     }
