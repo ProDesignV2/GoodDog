339a65ea3101edd16664452d821c740e8f772bca
ProDesignV2 <eoinpgogarty@gmail.com>
2019-06-11 16:47:09 +0100

    Loading icon included

--- app/src/main/java/com/avantgarda/gooddog/MainActivity.java
+++ app/src/main/java/com/avantgarda/gooddog/MainActivity.java
@@ -1,16 +1,25 @@
 package com.avantgarda.gooddog;
 
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.app.ActionBar;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -19,15 +28,21 @@ import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
 import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
-    ImageView imageView;
-    View dudLeft, dudRight;
+    ImageView imageView, pulseView;
 
-    private final static int REQUEST_IMAGE_CAPTURE = 0;
+    ObjectAnimator scaleDown;
+    String currentPhotoPath;
+
+    static final int REQUEST_TAKE_PHOTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,39 +51,42 @@ public class MainActivity extends AppCompatActivity {
 
         button = findViewById(R.id.button);
         imageView = findViewById(R.id.imageView);
-        dudLeft = findViewById(R.id.dudLeft);
-        dudRight = findViewById(R.id.dudRight);
+        pulseView = findViewById(R.id.pulseView);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if(intent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
-                }
+                dispatchTakePictureIntent();
             }
         });
 
         button.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                dudLeft.setVisibility(View.VISIBLE);
-                dudRight.setVisibility(View.VISIBLE);
                 imageView.setImageResource(R.drawable.corgi);
                 return true;
             }
         });
+
+        scaleDown = ObjectAnimator.ofPropertyValuesHolder(
+                pulseView,
+                PropertyValuesHolder.ofFloat("scaleX", 1.2f),
+                PropertyValuesHolder.ofFloat("scaleY", 1.2f));
+        scaleDown.setDuration(310);
+
+        scaleDown.setRepeatCount(ObjectAnimator.INFINITE);
+        scaleDown.setRepeatMode(ObjectAnimator.REVERSE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == REQUEST_IMAGE_CAPTURE){
+        if(requestCode == REQUEST_TAKE_PHOTO){
             if(resultCode == RESULT_OK){
-                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-                dudLeft.setVisibility(View.GONE);
-                dudRight.setVisibility(View.GONE);
+                loadingStart();
+                File imageFile = new File(currentPhotoPath);
+                Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
                 imageView.setImageBitmap(bitmap);
                 FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
                 FirebaseVisionImageLabeler labeler = FirebaseVision.getInstance()
@@ -83,6 +101,7 @@ public class MainActivity extends AppCompatActivity {
                                     String entityId = label.getEntityId();
                                     float confidence = label.getConfidence();
                                     Log.i("MLResult", "onSuccess: " + text + " " + entityId + " " + confidence);
+                                    loadingFinish();
                                 }
                             }
                         })
@@ -91,8 +110,62 @@ public class MainActivity extends AppCompatActivity {
                             public void onFailure(@NonNull Exception e) {
                                 // Task failed with an exception
                                 Log.i("MLResult", "onFailure: " + e.getMessage());
+                                loadingFinish();
                             }
                         });
+            }
+        }
+    }
+
+    private void loadingStart() {
+        imageView.setVisibility(View.GONE);
+        button.setVisibility(View.GONE);
+        pulseView.setVisibility(View.VISIBLE);
+        scaleDown.start();
+    }
+
+    private void loadingFinish() {
+        imageView.setVisibility(View.VISIBLE);
+        button.setVisibility(View.VISIBLE);
+        pulseView.setVisibility(View.GONE);
+        scaleDown.cancel();
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                // ...
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.avantgarda.gooddog.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
             }
         }
     }
