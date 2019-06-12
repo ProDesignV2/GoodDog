3bafe6336375f620ef67c2a6beb91a1244455186
ProDesignV2 <eoinpgogarty@gmail.com>
2019-06-12 14:27:53 +0100

    First release build

--- app/src/main/java/com/avantgarda/gooddog/MainActivity.java
+++ app/src/main/java/com/avantgarda/gooddog/MainActivity.java
@@ -3,6 +3,7 @@ package com.avantgarda.gooddog;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.app.ActionBar;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -13,13 +14,20 @@ import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -37,10 +45,14 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     Button button;
-    ImageView imageView, pulseView;
+    ImageView corgiView, pulseView, photoView;
+    TextView resultView;
 
     ObjectAnimator scaleDown;
     String currentPhotoPath;
+    String labelCheck, labelLower;
+
+    boolean isUp;
 
     static final int REQUEST_TAKE_PHOTO = 1;
 
@@ -50,12 +62,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
-        imageView = findViewById(R.id.imageView);
+        photoView = findViewById(R.id.photoView);
+        corgiView = findViewById(R.id.corgiView);
         pulseView = findViewById(R.id.pulseView);
+        resultView = findViewById(R.id.resultsView);
+
+        isUp = true;
+        labelCheck = "Dog";
+        labelLower = "dog";
+        button.setText(getString(R.string.find_good_dog, labelLower));
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                slideUp(resultView,true);
                 dispatchTakePictureIntent();
             }
         });
@@ -63,7 +83,18 @@ public class MainActivity extends AppCompatActivity {
         button.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                imageView.setImageResource(R.drawable.corgi);
+                corgiView.setVisibility(View.VISIBLE);
+                photoView.setVisibility(View.GONE);
+                slideUp(resultView,false);
+                return true;
+            }
+        });
+
+        corgiView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                // Change label
+                labelChange();
                 return true;
             }
         });
@@ -78,6 +109,38 @@ public class MainActivity extends AppCompatActivity {
         scaleDown.setRepeatMode(ObjectAnimator.REVERSE);
     }
 
+    private void labelChange() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Change Label");
+        builder.setMessage("Look for other things that are good?");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        input.setTextSize(32);
+        input.setPadding(32,32,32,32);
+        // Specify the type of input expected
+        input.setInputType(InputType.TYPE_TEXT_FLAG_CAP_WORDS);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                labelCheck = input.getText().toString();
+                labelLower = labelCheck.toLowerCase();
+                button.setText(getString(R.string.find_good_dog, labelLower));
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -87,7 +150,7 @@ public class MainActivity extends AppCompatActivity {
                 loadingStart();
                 File imageFile = new File(currentPhotoPath);
                 Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
-                imageView.setImageBitmap(bitmap);
+                photoView.setImageBitmap(bitmap);
                 FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
                 FirebaseVisionImageLabeler labeler = FirebaseVision.getInstance()
                         .getOnDeviceImageLabeler();
@@ -96,13 +159,15 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onSuccess(List<FirebaseVisionImageLabel> labels) {
                                 // Task completed successfully
+                                float labelConfidence = 0;
                                 for (FirebaseVisionImageLabel label: labels) {
                                     String text = label.getText();
                                     String entityId = label.getEntityId();
                                     float confidence = label.getConfidence();
+                                    if(labelCheck.equals(text)){ labelConfidence = confidence; }
                                     Log.i("MLResult", "onSuccess: " + text + " " + entityId + " " + confidence);
-                                    loadingFinish();
                                 }
+                                loadingFinish(true, labelConfidence);
                             }
                         })
                         .addOnFailureListener(new OnFailureListener() {
@@ -110,7 +175,7 @@ public class MainActivity extends AppCompatActivity {
                             public void onFailure(@NonNull Exception e) {
                                 // Task failed with an exception
                                 Log.i("MLResult", "onFailure: " + e.getMessage());
-                                loadingFinish();
+                                loadingFinish(false,0);
                             }
                         });
             }
@@ -118,17 +183,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadingStart() {
-        imageView.setVisibility(View.GONE);
+        corgiView.setVisibility(View.GONE);
+        photoView.setVisibility(View.GONE);
         button.setVisibility(View.GONE);
         pulseView.setVisibility(View.VISIBLE);
         scaleDown.start();
     }
 
-    private void loadingFinish() {
-        imageView.setVisibility(View.VISIBLE);
+    private void loadingFinish(boolean successful, float confidence) {
+        photoView.setVisibility(View.VISIBLE);
         button.setVisibility(View.VISIBLE);
         pulseView.setVisibility(View.GONE);
         scaleDown.cancel();
+        if(successful){
+            if(confidence <= 0.05){ resultView.setText(getString(R.string.not_dog, labelLower, labelLower)); }
+            else { resultView.setText(getString(R.string.is_good_dog, labelLower)); }
+            slideDown(resultView);
+        }
     }
 
     private File createImageFile() throws IOException {
@@ -168,5 +239,33 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
             }
         }
+    }
+
+    public void slideUp(View view, boolean hideQuick){
+        if(isUp){ return; }
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+                0,  // fromYDelta
+                -view.getHeight());                // toYDelta
+        animate.setDuration(500);
+        if(hideQuick){ animate.setDuration(0); }
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+        isUp = true;
+    }
+
+    public void slideDown(View view){
+        view.setVisibility(View.VISIBLE);
+        view.bringToFront();
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+                -view.getHeight(),                 // fromYDelta
+                0); // toYDelta
+        animate.setDuration(500);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+        isUp = false;
     }
 }
